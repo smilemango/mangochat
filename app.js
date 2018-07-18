50061440f6e26bc94d0a3619e7c0e3ee89ec7369
smilemango <smileman.god@gmail.com>
2018-07-18 22:57:50 +0900

    채팅방 입장과 간단한 기능 추가

--- app.js
+++ app.js
@@ -14,11 +14,10 @@ io.on('connection', function(socket) {
 
   // 접속한 클라이언트의 정보가 수신되면
   socket.on('login', function(data) {
-    console.log('Client logged-in:\n name:' + data.name + '\n userid: ' + data.userid);
+    console.log('Client logged-in:\n name:' + data.name + '\n');
 
     // socket에 클라이언트 정보를 저장한다
     socket.name = data.name;
-    socket.userid = data.userid;
 
     // 접속된 모든 클라이언트에게 메시지를 전송한다
     io.emit('login', data.name );
